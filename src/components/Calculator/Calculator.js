11b853a6d66df761ae3c5a5a73d4998a298862ef
clarice42 <claramaia820@gmail.com>
2021-09-29 21:06:06 -0300

    ✅ ✨ Test and implement updateDisplay function

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Calculator.css";
 import Display from "../Display/Display";
 import Keypad from "../Keypad/Keypad";
+import { act } from "react-dom/test-utils";
 
 export const Calculator = () => {
   const [displayValue, setDisplayValue] = useState("0");
@@ -31,9 +32,23 @@ export const Calculator = () => {
     console.log("set operation");
   };
 
-  const updateDisplay = () => {
-    console.log("update display");
-    setDisplayValue('9');
+  const updateDisplay = (value) => {
+    let actualValue = displayValue;
+    // prevent multiple occurences of '.'
+    if (value === "." && displayValue.includes(".")) value = "";
+
+    if (value === "ce") {
+      // deletes last char in displayValue
+      actualValue = displayValue.substr(0, displayValue.length - 1);
+      // set displayValue to '0' if displayValue is empty string
+      if (actualValue === "") actualValue = "0";
+    } else {
+      // replace displayValue with value if displayValue equal to '0'
+      // else concatenate displayValue and value
+      actualValue === "0" ? (actualValue = value) : (actualValue += value);
+    }
+
+    setDisplayValue(actualValue);
   };
 
   return (
--- src/components/Calculator/Calculator.test.js
+++ src/components/Calculator/Calculator.test.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import Calculator from "./Calculator";
 import Display from "../Display/Display";
 import Keypad from "../Keypad/Keypad";
+import { getByTestId } from "@testing-library/dom";
 
 describe("Calculator", () => {
   let wrapper;
@@ -28,3 +29,85 @@ describe("Calculator", () => {
     ).toEqual(true);
   });
 });
+
+describe("updateDisplay", () => {
+  let wrapper;
+
+  beforeEach(() => (wrapper = mount(<Calculator />)));
+
+  it("updates displayValue", () => {
+    expect(wrapper.find("Display").props().displayValue).toEqual("0");
+    wrapper.find(".number-key").first().simulate("click");
+    expect(wrapper.find("Display").props().displayValue).toEqual("9");
+  });
+
+  it("concatenates displayValue", () => {
+    wrapper.find(".number-key").first().simulate("click");
+    wrapper.find(".number-key").at(1).simulate("click");
+    expect(wrapper.find("Display").props().displayValue).toEqual("98");
+  });
+
+  it("removes leading '0' from displayValue", () => {
+    wrapper.find(".number-key").at(10).simulate("click");
+    expect(wrapper.find("Display").props().displayValue).toEqual("0");
+    wrapper.find(".number-key").at(1).simulate("click");
+    expect(wrapper.find("Display").props().displayValue).toEqual("8");
+  });
+
+  it("prevents multiple leading '0's from displayValue", () => {
+    wrapper.find(".number-key").at(10).simulate("click");
+    wrapper.find(".number-key").at(10).simulate("click");
+    expect(wrapper.find("Display").props().displayValue).toEqual("0");
+  });
+
+  it("removes last char of displayValue", () => {
+    wrapper.find(".number-key").at(1).simulate("click");
+    wrapper.find(".number-key").at(2).simulate("click");
+    wrapper.find(".number-key").at(11).simulate("click");
+    expect(wrapper.find("Display").props().displayValue).toEqual("8");
+  });
+
+  it("prevents multiple instances of '.' in displayValue", () => {
+    wrapper.find(".number-key").at(9).simulate("click");
+    wrapper.find(".number-key").at(9).simulate("click");
+    expect(wrapper.find("Display").props().displayValue).toEqual(".");
+  });
+
+  it("will set displayValue to '0' if displayValue is equal to an empty string", () => {
+    wrapper.find(".number-key").at(11).simulate("click");
+    expect(wrapper.find("Display").props().displayValue).toEqual("0");
+  });
+});
+
+// describe("setOperator", () => {
+//   let wrapper;
+
+//   beforeEach(() => (wrapper = shallow(<Calculator />)));
+
+//   it("updates the value of selectedOperator", () => {
+//     const x = getByTestId(wrapper, "selectedOperator");
+//     expect(x).toEqual("");
+//     wrapper.find(".operator-key").first().simulate("click");
+//     expect(wrapper.prop("selectedOperator")).toEqual("/");
+//   });
+
+//   it("updates the value of storedValue to the value of displayValue", () => {
+//     wrapper.setState({ displayValue: "5" });
+//     wrapper.instance().setOperator("+");
+//     expect(wrapper.state("storedValue")).toEqual("5");
+//   });
+
+//   it('updates the value of displayValue to "0"', () => {
+//     wrapper.setState({ displayValue: "5" });
+//     wrapper.instance().setOperator("+");
+//     expect(wrapper.state("displayValue")).toEqual("0");
+//   });
+
+//   it("selectedOperator is not an empty string, does not update storedValue", () => {
+//     wrapper.setState({ displayValue: "5" });
+//     wrapper.instance().setOperator("+");
+//     expect(wrapper.state("storedValue")).toEqual("5");
+//     wrapper.instance().setOperator("-");
+//     expect(wrapper.state("storedValue")).toEqual("5");
+//   });
+// });
